d3ce740247572d30f0a7a660b959045abef914b0
kaiser185 <camilo92005@gmail.com>
2020-12-03 22:34:13 +0100

    stable for merging

--- frontend/single-spa.config.js
+++ frontend/single-spa.config.js
@@ -3,13 +3,13 @@ import { registerApplication, start } from 'single-spa'
 registerApplication(
   'vue', 
   () => import('./src/vue/vue.app.js'),
-  () => location.pathname === "/react" ? false : true
+  () => location.pathname === "/vue" ? false : true
 );
 
 registerApplication(
   'react',
   () => import('./src/react/main.app.js'),
-  () => location.pathname === "/vue"  ? false : true
+  () => location.pathname === "/react"  ? false : true
 );
 
 start();
--- frontend/src/vue/main.vue
+++ frontend/src/vue/main.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
       <h1>Hello from Vue</h1>
+      <GMaps/>
   </div>
 </template>
+
+<script>
+export default {
+  components:{
+      GMaps
+  },
+}
+</script>
